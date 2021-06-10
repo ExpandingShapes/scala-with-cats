2ac75b284ca6ce705dc04b69822b6146bfc2ea99
Alexander <lapseofreason17@gmail.com>
2021-06-10 17:12:34 +0300

    1.4.5 - Defining Custom Instances

--- src/main/scala/meetcats/MyApp.scala
+++ src/main/scala/meetcats/MyApp.scala
@@ -1,5 +1,7 @@
 package meetcats
 
+import java.util.Date
+
 import cats.Show
 import cats.instances.int._
 import cats.instances.string._
@@ -15,4 +17,10 @@ object MyApp extends App {
   //make Show easier to use by importing the interface syntax
   val showIntV2 = 123.show
   val showStringV2 = "abc".show
+  
+  //We can define an instance ofShowsimply by implementing the trait for a giventype:
+  //implicitvaldateShow: Show[Date] =newShow[Date] {defshow(date: Date): String =s"${date.getTime}mssincetheepoch."}
+  
+  //Cats also provides a couple of convenient methods to simplify theprocess. There are two construction methods on the companion object ofShowthat we can use to define instances for our own types:
+  implicit val dateShow: Show[Date] =Show.show(date => s"${date.getTime}ms since the epoch.")
 }
