b6901d4fcafa9cb8338fa3b9dda6d86bdf6c0222
Alexander Boyadzhi <lapseofreason17@gmail.com>
2021-06-12 16:56:27 +0300

    1.7 | summary

---
+++ src/main/scala/exampleeq/exercise/MyApp.scala
@@ -0,0 +1,23 @@
+package exampleeq.exercise
+
+import cats.Eq
+import cats.implicits._
+import cats.instances.int._
+import cats.syntax.eq._
+
+object MyApp extends App {
+  implicit val catEq: Eq[Cat] = Eq.instance[Cat] { (cat1, cat2) =>
+    cat1.age === cat2.age &&
+    cat1.name === cat2.name &&
+    cat1.color === cat2.color
+  }
+
+  val cat1 = Cat("Garfield", 38, "orange and black")
+  val cat2 = Cat("Heathcliff", 33, "orange and black")
+
+  val optionCat1 = cat1.some
+  val optionCat2 = Option.empty[Cat]
+
+  cat1 === cat2
+  optionCat1 =!= optionCat2
+}
