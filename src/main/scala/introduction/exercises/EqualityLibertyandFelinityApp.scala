0f453dc4c94541d1264d2b9e0977fa073c1f6ae3
Бояджи Александр <boyadzhi@kinoplan.ru>
2022-09-26 23:29:07 +0300

    2 - Monoids and Semigroups

---
+++ src/main/scala/introduction/exercises/EqualityLibertyandFelinityApp.scala
@@ -0,0 +1,23 @@
+package introduction.exercises
+
+import cats.Eq
+import cats.syntax.eq._
+
+//1.5.5
+object EqualityLibertyandFelinityApp extends App {
+  implicit val catEq: Eq[Cat] =
+    Eq.instance[Cat]{(cat1, cat2) =>
+      cat1.name === cat2.name &&
+      cat1.age === cat2.age &&
+      cat1.color === cat2.color
+    }
+
+  val cat1 = Cat("Garfield", 38, "orange and black")
+  val cat2 = Cat("Heathcliff", 33, "orange and black")
+
+  val optionCat1 = Option(cat1)
+  val optionCat2 = Option.empty[Cat]
+
+  cat1 === cat2
+  optionCat1 === optionCat2
+}
