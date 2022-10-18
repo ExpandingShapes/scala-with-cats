1f2847ac0983edc9b5ffda9d3b34291be01557ae
Бояджи Александр <boyadzhi@kinoplan.ru>
2022-10-18 22:07:08 +0300

    3.3 - Definition of a Functor

--- src/main/scala/functors/examples/FunctorsChapter.scala
+++ src/main/scala/functors/examples/FunctorsChapter.scala
@@ -3,6 +3,7 @@ package functors.examples
 import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
+import cats.syntax.functor._
 
 object FunctorsChapter extends App {
   val future: Future[String] =
@@ -11,5 +12,21 @@ object FunctorsChapter extends App {
       .map(n => n * 2)
       .map(n => s"${n}!")
 
-  print(Await.result(future, 1.second))
+  Await.result(future, 1.second)
+
+  //mapping over Function1 is a function composition
+  val func1: Int => Double = (x: Int) => x.toDouble
+  val func2: Double => Double = (y: Double) => y * 2
+
+  //the composition itself
+  (func1 map func2)(1)
+  (func1 andThen func2)(1)
+  func2(func1(1))
+
+  //We can think of this as lazily queueing up operations similar to Future
+  val func = ((x: Int) => x.toDouble)
+    .map(x => x + 1)
+    .map(x => x * 2)
+    .map(x => s"${x}!")
+
 }
