33e7d4a3d387d3ab1871cca03859e1ad22aaad2f
Бояджи Александр <boyadzhi@kinoplan.ru>
2022-10-19 23:59:10 +0300

    3.5.2 - Functor Syntax

--- src/main/scala/functors/examples/FunctorsChapter.scala
+++ src/main/scala/functors/examples/FunctorsChapter.scala
@@ -1,5 +1,7 @@
 package functors.examples
 
+import cats.Functor
+
 import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
@@ -29,4 +31,24 @@ object FunctorsChapter extends App {
     .map(x => x * 2)
     .map(x => s"${x}!")
 
+  val list1 = List(1, 2, 3)
+  val list2 = Functor[List].map(list1)(_ * 2)
+
+  val option1 = Option(123)
+  val option2 = Functor[Option].map(option1)(_.toString)
+
+  val func3 = (x: Int) => x + 1
+  val liftedFunc3 = Functor[Option].lift(func3)
+  liftedFunc3(Option(1))
+
+  Functor[List].as(list1, "As")
+
+  /**
+   * Apply an equation to a number no matter what functor context it’s in
+   */
+  def doMath[F[_]](start: F[Int])(implicit functor: Functor[F]): F[Int] =
+    start.map(n => n + 1 * 2)
+
+  doMath(Option(20))
+  doMath(List(1, 2, 3))
 }
